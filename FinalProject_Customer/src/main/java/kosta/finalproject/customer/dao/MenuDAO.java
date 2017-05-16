1ede040c946d1f22f01de62983bf687b4ccdbde1
STUDYHARD\Chanhyeong <mnbv1686@naver.com>
2017-05-16 15:08:03 +0900

    나도 바꿧다

--- FinalProject_Customer/src/main/java/kosta/finalproject/customer/dao/MenuDAO.java
+++ FinalProject_Customer/src/main/java/kosta/finalproject/customer/dao/MenuDAO.java
@@ -1,9 +1,21 @@
 package kosta.finalproject.customer.dao;
 
+import java.util.List;
+
+import kosta.finalproject.customer.dto.MenuTDTO;
+
 public interface MenuDAO {
 	//이것은 메뉴 DAO 인터페이스입니다.
 	//작성자 : 이찬형 
 	//현재시각 14:54
 	
 
+	
+	
+	
+	
+	public List<MenuTDTO> testList();
+	
+	
+	
 }
