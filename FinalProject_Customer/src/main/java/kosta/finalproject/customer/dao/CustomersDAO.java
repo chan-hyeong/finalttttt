b320d1516e1c881fa9fd580b8d1e0b897ba85556
STUDYHARD\Chanhyeong <mnbv1686@naver.com>
2017-05-16 14:55:54 +0900

    menuDAO 인터페이스를 만들어봤음

--- FinalProject_Customer/src/main/java/kosta/finalproject/customer/dao/CustomersDAO.java
+++ FinalProject_Customer/src/main/java/kosta/finalproject/customer/dao/CustomersDAO.java
@@ -1,5 +1,10 @@
 package kosta.finalproject.customer.dao;
 
+import kosta.finalproject.customer.dto.CustomersTDTO;
+
 public interface CustomersDAO {
+	
+	
+	public void Join(CustomersTDTO newCustomer);
 
 }
---
+++ FinalProject_Customer/src/main/java/kosta/finalproject/customer/dao/MenuDAO.java
@@ -0,0 +1,9 @@
+package kosta.finalproject.customer.dao;
+
+public interface MenuDAO {
+	//이것은 메뉴 DAO 인터페이스입니다.
+	//작성자 : 이찬형 
+	//현재시각 14:54
+	
+
+}
