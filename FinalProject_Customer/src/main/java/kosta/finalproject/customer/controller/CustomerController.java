040a149929fdad4509105d0221c0fd9b33997501
KOSTA-HP\Administrator <swdjdoc@naver.com>
2017-05-16 15:42:25 +0900

    ㅁㄴㅇ

--- FinalProject_Customer/src/main/java/kosta/finalproject/customer/controller/CustomerController.java
+++ FinalProject_Customer/src/main/java/kosta/finalproject/customer/controller/CustomerController.java
@@ -7,20 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CustomerController {
-	/*주석을 존나 달아야자ㅣ 
-	버거킹가고싶다 빨리 
-	새우버거*/
-	
-	
-	/*나도 수정함 되겠지*/
-	
-	/*다시해보자*/
-	
-	/*아 준혁이*/
+	/*
+	 * 주석을 존나 달아야자ㅣ 버거킹가고싶다 빨리 새우버거
+	 */
+
+	/* 나도 수정함 되겠지 */
+
+	/* 다시해보자 */
+
+	/* 아 준혁이 */
 	@Autowired
 	private SqlSession sqlsessoin;
-	
+
 	@RequestMapping("/index.do")
+<<<<<<< HEAD
 	public String index(){
 		
 
@@ -32,68 +32,72 @@ public class CustomerController {
 
 
 System.out.println("나도 이거 작업중 tttt");
+=======
+	public String index() {
 
+		int i = 100;
+		System.out.println("이건 이찬형이 쓴 메시지이다");
+		System.out.println("병수가 쓴건 과연 어디에?" + i);
+		
+		System.out.println("이건 수정한 내용이다");
+		
+>>>>>>> 2e077a267e354de9c240ce47417facdc93923230
 
-
-
-System.out.println("병수가 작업 한 내용 이다 26번줄 ");
+		System.out.println("병수가 작업 한 내용 이다 26번줄 ");
 		return "customer/index";
 	}
-	
+
 	@RequestMapping("/loginform.do")
-	public String loginform(){
-		
-		
+	public String loginform() {
+
 		return "customer/loginform";
 	}
-	
+
 	@RequestMapping("/joinform.do")
-	public String joinform(){
-		
-		
+	public String joinform() {
+
 		return "customer/joinform";
 	}
-	
+
 	@RequestMapping("/main.do")
-	public String main(){
-		
+	public String main() {
+
 		return "customer/main";
 	}
-	
+
 	@RequestMapping("/menulist.do")
-	public String menulist(){
-		
+	public String menulist() {
+
 		return "customer/menulist";
 	}
-	
+
 	@RequestMapping("/favorite.do")
-	public String favorite(){
-		
+	public String favorite() {
+
 		return "customer/favorite";
 	}
-	
+
 	@RequestMapping("/history.do")
-	public String history(){
-		
+	public String history() {
+
 		return "customer/history";
 	}
-	
-		@RequestMapping("/cash.do")
-	public String cash(){
-		
+
+	@RequestMapping("/cash.do")
+	public String cash() {
+
 		return "customer/cashform";
 	}
-		
-			@RequestMapping("/orderdetail.do")
-		public String orderdetail(){
-			
-			return "customer/orderdetail";
-		}
-			
-			
-		@RequestMapping("/shoppingbag.do")
-			public String shoppingbag(){
-				
-				return "customer/shoppingbag";
-			}
+
+	@RequestMapping("/orderdetail.do")
+	public String orderdetail() {
+
+		return "customer/orderdetail";
+	}
+
+	@RequestMapping("/shoppingbag.do")
+	public String shoppingbag() {
+
+		return "customer/shoppingbag";
+	}
 }
